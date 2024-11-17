9772db43a3d4c3af6d868b22a15cd8fc5af3a29f
ackwolver335 <abhaych335@gmail.com>
2024-11-17 18:55:01 +0530

    Feature - Added Language Detector, Clear Entry,Auto Close Window, Unique Record

--- source-code/main_code.py
+++ source-code/main_code.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-import tkinter.messagebox as mg
+import tkinter.messagebox as mg                                 # module for showing messages and warning box
+from lingua import Language, LanguageDetectorBuilder            # language detector module
 
 # major important modules
 from transformers import pipeline,DistilBertTokenizer,DistilBertForSequenceClassification
@@ -16,45 +17,105 @@ emotion_analyzer = pipeline('text-classification',model = 'j-hartmann/emotion-en
 
 w1 = tk.Tk()
 w1.title("Sentiment-Analyzer")
-w1.geometry('400x220')
+w1.geometry('400x250')
 w1.resizable(False,False)
 w1.config(background = '#d9d9d9')
 
-# general message when the Application starts
-mg.showinfo('Application General Information','It is general notice regarding the Application that it is used for getting the score Sentiment and Emotion calculated after you give your text based content in the input prompt.')
+# initating a textual list for not entry creating a window for same text
+textual_content = []
 
 # creating a label here
 lb1 = tk.Label(w1,text = 'Sentiment Analyzer',font = ('Bitstream Charter',15,'bold'))
 lb1.pack(padx = 2 ,pady = 5)
 
 # creating an entry point here
-e1 = tk.Entry(w1,width = 30,font = ('Bitstream Charter',12,'italic'),border = None)
-e1.pack(padx = 2,pady = 5)
+e1 = tk.Entry(w1,width = 30,font = ('Bitstream Charter',12),border = None)
+e1.pack(padx = 2,pady = 4)
+
+# method in order to clear the entry box
+def clear_entry():
+    if e1.get() == '':
+        mg.showwarning('Application Warning','The Entry Box is already cleared !')
+    else:
+        e1.delete(0,tk.END)
+
+# creating a button to make the entry empty if the user wants to
+btn = tk.Button(text = 'Clear All',font = ('Bitstream Charter',10),command = clear_entry,border = None)
+btn.pack(padx = 2,pady = 3)
+
+# building language detector for english language
+detector = LanguageDetectorBuilder.from_languages(Language.ENGLISH,Language.GERMAN).build()
+
+# method for closing the main window
+def close_main():
+    w1.destroy()
 
 # function's or Method's Block to be used as a command afterwards
 def doThis():
-    
-    # initiating the second window
-    w2 = tk.Tk()
-    w2.title("Analysed Sentiment")
-    w2.geometry('300x150')
-    w2.resizable(False,False)
 
-    # initiative label
-    wlb1 = tk.Label(w2,text = "Analysed Sentiment",font = ('Bitstream Charter',13,'bold'))
-    wlb1.pack(padx = 2,pady = 5)
+    # block for the duplication of sentiment data
+    if e1.get() in textual_content:
+        mg.showwarning('Application Warning','The Textual Sentiment line is already available in our Database')
+        return
 
-    result = analyzer(e1.get())
-    emotion = emotion_analyzer(e1.get())
+    # adding the data to the list if not available
+    if e1.get() not in textual_content:
+        textual_content.append(e1.get())
 
-    # final detailed label
-    final_output = 'Your Sentiment Label : {0}\nYour Sentiment Score : {1}%\nYour emotion : {2}'.format(result[0]['label'],int(result[0]['score'] * 100),(emotion[0]['label'].capitalize()))
+    # situation when the user wants his/her sentiment detail and forget to enter the text
+    if e1.get() == '':
+        mg.showwarning('Application Warning','You are trying to get the data without providing the textual data !')
+        return
 
-    wlb2 = tk.Label(w2,anchor = tk.CENTER,text = final_output,font = ('Bitstream Charter',10))
-    wlb2.pack(padx = 2,pady = 2)
+    elif detector.detect_language_of(e1.get()) == Language.ENGLISH:
+        
+        # initiating the second window
+        w2 = tk.Tk()
+        w2.title("Analysed Sentiment")
+        w2.geometry('300x120')
+        w2.resizable(False,False)
 
-    # ending of the window
-    w2.mainloop()
+        # initiative label
+        wlb1 = tk.Label(w2,text = "Analysed Sentiment",font = ('Bitstream Charter',13,'bold'))
+        wlb1.pack(padx = 2,pady = 5)
+
+        result = analyzer(e1.get())
+        emotion = emotion_analyzer(e1.get())
+
+        # final detailed label
+        final_output = 'Your Sentiment Label : {0}\nYour Sentiment Score : {1}%\nYour emotion : {2}'.format(result[0]['label'],int(result[0]['score'] * 100),(emotion[0]['label'].capitalize()))
+
+        wlb2 = tk.Label(w2,anchor = tk.CENTER,text = final_output,font = ('Bitstream Charter',10))
+        wlb2.pack(padx = 2,pady = 2)
+
+        # Dark Mode according to the initial window
+        if var1.get() == 1:
+            w2.config(background = '#292826')
+            wlb1.config(bg = '#292826',fg = 'white')
+            wlb2.config(bg = '#292826',fg = 'white')
+
+        # method for closing the window after specific time
+        def close_window():
+            w2.destroy()
+
+        # timer for reseting window from user interaction
+        def reset_window(event = None):
+            w2.after_cancel(timer[0])
+            timer[0] = w2.after(10000,close_window)
+
+        # making a time for this purpose
+        timer = [w2.after(10000,close_window)]
+
+        # binding events to reset when user interact again
+        w2.bind('<Motion>',reset_window)
+        w2.bind('<KeyPress>',reset_window)
+        w2.bind('<Button>',reset_window)                
+
+        # ending of the window
+        w2.mainloop()
+
+    else:
+        mg.warning('Application Warning','Trying to use another language, only english is allowed !')
 
 def dark_mode():
 
@@ -71,6 +132,7 @@ def dark_mode():
         flb2.config(bg = '#292826',fg = 'white')
 
         # main button configuration
+        btn.config(bg = '#292826',fg = 'white')
         btn1.config(bg = '#292826',fg = 'white')
     
     # disable dark mode configuration
@@ -86,6 +148,7 @@ def dark_mode():
         flb2.config(bg = '#d9d9d9',fg = 'black')
 
         # main button configuration
+        btn.config(bg = '#d9d9d9',fg = 'black')
         btn1.config(bg = '#d9d9d9',fg = 'black')
 
 # adding frame for getting example
@@ -109,6 +172,20 @@ chb1.pack(side = 'left',padx = 4,pady = 1)
 
 # creating a button here
 btn1 = tk.Button(w1,text = 'Submit Text to Analyse',border = None,command = doThis)
-btn1.pack(side = 'right',padx = 4,pady = 1) 
+btn1.pack(side = 'right',padx = 4,pady = 1)
+print(textual_content)
+
+# timer for reseting window from user interaction
+def reset_window_time(event = None):
+    w1.after_cancel(timer[0])
+    timer[0] = w1.after(20000,close_main)
+
+# making a time for this purpose
+timer = [w1.after(20000,close_main)]
+
+# binding events to reset when user interact again
+w1.bind('<Motion>',reset_window_time)
+w1.bind('<KeyPress>',reset_window_time)
+w1.bind('<Button>',reset_window_time)
 
 w1.mainloop()
